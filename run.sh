75c8909de35370736d3d0ca1fd08867d6d409a97
OrestSonich <sonich.work@gmail.com>
2024-11-13 15:52:13 +0200

    chore: delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run Flask API server
-python -m src.main &
-
-# Run LiveKit agent
-python -c "from src.voice_agent import entrypoint, prewarm; from livekit.agents import cli, WorkerOptions; cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, prewarm_fnc=prewarm))" dev
